25d3434894b94b180eb34271a966f1482a162cdf
matszal <mszalkowski2011@gmail.com>
2019-04-08 21:05:41 +0100

    Added game class
    Squashed useless commit
    
    Added game object class

--- GameSetup.py
+++ GameSetup.py
@@ -5,34 +5,68 @@
 import pygame
 
 # screen size
+SCREEN_TITLE = "My pygame Game"
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 1000
+
 # color rgb representation
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
 
 # clock setup
 clock = pygame.time.Clock()
-# tick rate
-TICK_RATE = 60
-# continue game
-is_game_over = False
 
-# create window of specific size
-game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+class Game:
+    # refresh rate
+    TICK_RATE = 60
 
-# setup window background color
-game_window.fill(WHITE_COLOR)
+    def __init__(self, title, width, height ):
+        self.title = title
+        self.width = width
+        self.height = height
+        # create window of specific size
+        self.game_window = pygame.display.set_mode((width, height))
 
-# render all the graphics
-while not is_game_over:
+        # setup window background color
+        self.game_window.fill(WHITE_COLOR)
+        pygame.display.set_caption(title)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_game_over = True
+    def run_game_loop(self):
+        is_game_over = False
+        # render all the graphics
+        while not is_game_over:
 
-    pygame.display.update()
-    clock.tick(TICK_RATE)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+        
+                print(event)
+
+            pygame.display.update()
+            clock.tick(self.TICK_RATE)
+
+class GameObject:
+
+    def __init__(self, image_path, x, y, width, height):
+        object_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(object_image, [width, height])
+        self.x_pos = x
+        self.y_pos = y
+        self.width = width
+        self.height = height
+
+    def draw(self, background):
+        background.blit(self.image, (self.x_pos, self.y_pos))
+
+#class Player(GameObject):
+
+
+pygame.init()
+
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+gameObject = GameObject('player.png', 150, 150, 50, 50)
+#gameObject.draw(gameObject)
+new_game.run_game_loop()
 
 pygame.quit()
 quit
