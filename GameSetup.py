ed22cda0ac93f22929b869182f5c2d921bad04e2
matszal <mszalkowski2011@gmail.com>
2019-04-09 20:08:07 +0100

    Added basic player movement
    Now player is able to control character movement with keyboard

--- GameSetup.py
+++ GameSetup.py
@@ -16,6 +16,7 @@ BLACK_COLOR = (0, 0, 0)
 # clock setup
 clock = pygame.time.Clock()
 
+# game setup class
 class Game:
     # refresh rate
     TICK_RATE = 60
@@ -33,18 +34,34 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
+
+        player_character = Player('player.png', 325, 800, 150, 150)
         # render all the graphics
         while not is_game_over:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_game_over = True
-        
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0        
                 print(event)
+
+            self.game_window.fill(WHITE_COLOR)
+            player_character.move(direction)
+            player_character.draw(self.game_window)
 
             pygame.display.update()
             clock.tick(self.TICK_RATE)
 
+
+# class to represent game object
 class GameObject:
 
     def __init__(self, image_path, x, y, width, height):
@@ -58,15 +75,26 @@ class GameObject:
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
 
-#class Player(GameObject):
+# class to represent character controlled by the player
+class Player(GameObject, object):
+
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super(Player, self).__init__(image_path, x, y, width, height)
+        
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
 
 
 pygame.init()
 
 new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
-gameObject = GameObject('player.png', 150, 150, 50, 50)
-#gameObject.draw(gameObject)
 new_game.run_game_loop()
 
 pygame.quit()
+
 quit
